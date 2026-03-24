bac45eda5704e5a396182da0f6a851b272303d31
Benjamin Caballero <benjamin.caballero@externos-cl.cencosud.com>
2026-03-24 17:47:46 +0100

    fix: ran terraform fmt versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.6.0"
 
   backend "s3" {
-    bucket         = "finops-ai-advisor-tfstate-110735825932"
-    key            = "global/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "finops-ai-advisor-tfstate-110735825932"
+    key          = "global/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 
   required_providers {
