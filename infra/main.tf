5ebe917bd53ebebe6bc9e5e809d053909d99a615
Benjamin Caballero <benjamin.caballero@externos-cl.cencosud.com>
2026-03-25 15:44:43 +0100

    fix: comment max concurency and add skip rule to checkov

--- infra/main.tf
+++ infra/main.tf
@@ -68,7 +68,7 @@ resource "aws_lambda_function" "finops_analyzer" {
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   timeout                        = 10
-  reserved_concurrent_executions = 1
+  #reserved_concurrent_executions = 10
 
   tracing_config {
     mode = "Active"
